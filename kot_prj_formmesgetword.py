dc04fb4e1df74620a0408c2c6b1997724a0b1d78
Alexey Smirnov <aasmr@ya.ru>
2021-08-11 20:17:28 +0300

    dict

--- kot_prj_formmesgetword.py
+++ kot_prj_formmesgetword.py
@@ -108,7 +108,7 @@ if __name__=='__main__':
     except:
         word_dict_new=[]
     for i in word_dict:
-        if word_dict.index(i)<22565:
+        if word_dict.index(i)<25152:
             continue
         if dict.check(i)==False:
             print(i+'\n')
